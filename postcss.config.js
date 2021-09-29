0d1d7e2d8f50698a06a24dede635cc201b57cdc1
joao victor <joaovictor@agencia-flow.com>
2021-09-29 19:34:19 -0300

    New: url's for nano css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 const CSSNANO = require("cssnano")({ preset: "default" });
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./*.js", "./*.css", "./*.html"],
+  content: [
+    "./*.js",
+    "./*.css",
+    "./*.html",
+    "./**/*.js",
+    "./**/*.css",
+    "./**/*.html",
+  ],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
