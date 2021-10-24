76b323c5def03e2c16d46d59821db29f0f72a4ac
gustavo1 <gf9068586@gmail.com>
2021-10-24 20:26:09 -0300

    firs version page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      color:{
+        gray:{
+         100: '#F1F1F1'
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
